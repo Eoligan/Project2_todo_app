caf7512bc4520068239c9805446f3ba57d5cd752
Eoligan <106335980+Eoligan@users.noreply.github.com>
2023-08-07 12:02:04 +0000

    first view

---
+++ src/components/LoginBox.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  
+  <section class="grid place-items-center">
+    <div class="rounded-md bg-slate-300 px-8 pb-8 pt-4">
+      <h2 class="text-center text-xl font-bold m-4">Login</h2>
+      <div class="flex flex-col">
+        <!-- <label class="mt-6 place-self-start" for="email">Email</label> -->
+        <input
+          placeholder=" Email"
+          class="border-b-2 my-2 pb-1 border-slate-800 bg-slate-300 placeholder:text-slate-800"
+          id="email"
+          name="email"
+          type="email"
+        />
+        <!-- <label for="pass">Password</label> -->
+        <input placeholder=" Password"
+          class="border-b-2 my-2 pb-1 border-slate-800 bg-slate-300 placeholder:text-slate-800"
+          id="pass" name="pass" type="password" />
+
+        <button class="bg-amber-600 mt-3 rounded-md font-bold text-white">Login</button>
+        <a class="text-xs mt-3 text-amber-600 ">Don't have an account? Sign up</a>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/views/LandingView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import LoginBox from '@/components/LoginBox.vue';
+
+</script>
+
+<template>
+  <header class="m-16 flex flex-col gap-4">
+    <h1
+      class="bg-gradient-to-r from-amber-600 to-amber-900 bg-clip-text text-center text-6xl font-black uppercase tracking-widest text-transparent"
+    >
+      There's a lot <br />
+      To-Do
+    </h1>
+  </header> 
+  <main>
+    <LoginBox></LoginBox>
+  </main>
+</template>
