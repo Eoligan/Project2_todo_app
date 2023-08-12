a1215a07fdb8da2b1d0b79ff6afdc392fe7de3a0
Eoligan <sergio.hoyo.45@gmail.com>
2023-08-12 20:02:03 +0200

    added edit. basic functionality working

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,23 +1,52 @@
 <script setup async>
 import { useUserStore } from "@/stores/user"
 import { useTaskStore } from "@/stores/task"
-import { onMounted, ref } from "vue"
+import { onMounted, ref, computed, nextTick } from "vue"
 
 const userStore = useUserStore()
 const taskStore = useTaskStore()
 
 const userName = userStore.user.user.user_metadata.name
 const userId = userStore.user.user.id
-let taskTitle = ref()
+
+let taskTitle = ref("")
+const editingTask = ref(null)
 
 onMounted(async () => {
-  taskStore.fetchTasks()
+  try {
+    await taskStore.fetchTasks()
+    editingTask.value = Array(taskStore.tasks.length).fill(false)
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 const addTask = async () => {
   const added = await taskStore.addTask(userId, taskTitle.value)
   if (added) {
     taskTitle.value = ""
+  }
+}
+
+const sortedTasks = computed(() => {
+  const tasks = [...taskStore.tasks]
+  tasks.sort((a, b) => a.id - b.id)
+
+  return tasks
+})
+
+const toggleEditing = (task) => {
+  if (editingTask.value === task) {
+    editingTask.value = null
+    taskStore.editTask(task.id, task.title)
+  } else {
+    editingTask.value = task
+
+    nextTick(() => {
+      const input = document.getElementById("editInput")
+      input.focus()
+      input.select()
+    })
   }
 }
 </script>
@@ -36,20 +65,34 @@ const addTask = async () => {
   <h1 class="text-7xl">Task</h1>
 
   <ul>
-    <li
-      @click="taskStore.completedTask(task.id, task.is_completed)"
-      v-for="task in taskStore.tasks"
-    >
-      <p v-if="task.is_completed" class="bg-gray-100 text-slate-400 line-through">
+    <li v-for="task in sortedTasks" :key="task.id">
+      <div v-if="editingTask === task">
+        <input id="editInput" v-model="task.title" type="text" />
+      </div>
+      <p
+        v-else
+        @click="taskStore.completedTask(task.id, task.is_completed)"
+        :class="{
+          'bg-gray-100 text-slate-400 line-through': task.is_completed,
+          'bg-gray-400': !task.is_completed,
+        }"
+      >
         {{ task.title }} {{ task.is_completed }}
       </p>
-      <p v-else class="bg-gray-100">{{ task.title }} {{ task.is_completed }}</p>
-      <!-- <button
-        @click="taskStore.editTask(task.id, title)"
-        class="rounded bg-green-700 text-white"
+      <button
+        v-if="editingTask !== task"
+        @click="toggleEditing(task)"
+        class="rounded bg-amber-400 text-black"
       >
         edit
-      </button> -->
+      </button>
+      <button
+        v-else
+        @click="toggleEditing(task)"
+        class="rounded bg-green-700 text-white"
+      >
+        ok
+      </button>
       <button
         @click="taskStore.deleteTask(task.id)"
         class="rounded bg-red-700 text-white"
